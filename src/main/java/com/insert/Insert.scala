d400c375d163dba29b85126e236ff68d9e8cf417
antim098 <40453155+antim098@users.noreply.github.com>
2021-02-11 21:26:27 +0530

    changes

---
+++ src/main/java/com/insert/Insert.scala
@@ -0,0 +1,15 @@
+package com.insert
+
+import org.apache.spark.sql.DataFrame
+
+import scala.collection.JavaConversions._
+
+object Insert {
+
+  val insert = (df: DataFrame, keySpace: String, tableName: String) => {
+    val columnNames = df.columns.toList
+    CassandraDriverInsert.createConnection();
+    // testdf.rdd.foreach(row => driverInsert.insert("test", "spark_test1", columnNames,row.toSeq.asInstanceOf[Seq[Object]].toList, true))
+    df.foreach(row => CassandraDriverInsert.insert(keySpace, tableName, columnNames, row.toSeq.asInstanceOf[Seq[Object]].toList, true))
+  }
+}
