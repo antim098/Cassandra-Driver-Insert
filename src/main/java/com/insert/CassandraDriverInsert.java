01d5847708287db64823fabec23bfe2d9cd19d57
antim098 <40453155+antim098@users.noreply.github.com>
2021-02-16 18:59:20 +0530

    new

--- src/main/java/com/insert/CassandraDriverInsert.java
+++ src/main/java/com/insert/CassandraDriverInsert.java
@@ -100,10 +100,12 @@ public class CassandraDriverInsert implements Serializable {
 
     public static synchronized void executeBatchAsync(Session session) {
         if (BoundStatementList.size() == 10000) {
-            CopyOnWriteArrayList<BoundStatement> executionList = new CopyOnWriteArrayList<>(BoundStatementList.subList(0, 10000));
-            BoundStatementList.subList(0, 10000).clear();
-            for (BoundStatement statement : executionList) {
-                session.executeAsync(statement);
+            synchronized (BoundStatementList) {
+                CopyOnWriteArrayList<BoundStatement> executionList = new CopyOnWriteArrayList<>(BoundStatementList.subList(0, 10000));
+                BoundStatementList.subList(0, 10000).clear();
+                for (BoundStatement statement : executionList) {
+                    session.executeAsync(statement);
+                }
             }
         }
         //LOGGER.info("[" + CassandraDriverInsert.class.getName() + "] Processed 10,000 records");
